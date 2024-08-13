c15b5f18966347f512b0be86964be0e3005669b8
Yair Ashkenazi <yairush88@gmail.com>
2024-08-13 17:43:03 +0300

    Added files

---
+++ TriviaMaster.App/Question.cs
@@ -0,0 +1,9 @@
+﻿namespace TriviaMaster.App
+{
+    public class Question(string text, List<string> answers, int correctAnswerIndex)
+    {
+        public string Text { get; } = text;
+        public List<string> Answers { get; } = answers;
+        public int CorrectAnswerIndex { get; } = correctAnswerIndex;
+    }
+}
